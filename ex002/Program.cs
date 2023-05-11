14fccfb40ce104cad5a4dc77fe21a260f8dd7c41
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-05-11 10:51:18 -0300

    Add files via upload

---
+++ ex002/Program.cs
@@ -0,0 +1,8 @@
+﻿//2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas para ela:
+// Ex:
+// Qual é o seu nome? João da Silva
+// Olá João da Silva, é um prazer te conhecer!
+
+Console.WriteLine("Qual é o seu nome?");
+string nome = Console.ReadLine();
+Console.WriteLine("Oi, {0}, é um prazer te conhecer!",nome);
---
+++ ex003/Program.cs
@@ -0,0 +1,11 @@
+﻿// 3) Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
+// Ex:
+// Nome do Funcionário: Maria do Carmo
+// Salário: 1850,45
+// O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.
+
+Console.WriteLine("Qual o nome do funcionário?: ");
+string nome = Console.ReadLine();
+Console.WriteLine("Qual é o seu salário?: ");
+float salario = float.Parse(Console.ReadLine());
+Console.WriteLine("{0} recebe R${1}.", nome, salario);
