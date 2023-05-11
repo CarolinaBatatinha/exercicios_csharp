ac1f820195bd334f9c72d09b38155e6d12941e10
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-05-11 11:01:33 -0300

    Add files via upload

---
+++ ex005/Program.cs
@@ -0,0 +1,16 @@
+﻿// 5) Faça um programa que leia as duas notas de um aluno em uma matéria e mostre
+// na tela a sua média na disciplina.
+// Ex:
+// Nota 1: 4.5
+// Nota 2: 8.5
+// A média entre 4.5 e 8.5 é igual a 6.5
+
+Console.WriteLine("Digite sua primeira nota: ");
+double n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite sua segunda nota: ");
+double n2 = double.Parse(Console.ReadLine());
+
+double media = (n1 + n2)/2;
+
+Console.WriteLine("A média entre {0} e {1} é igual a {2}.", n1, n2, media);
+Console.ReadLine();
---
+++ ex006/Program.cs
@@ -0,0 +1,13 @@
+﻿// 6) Faça um programa que leia um número inteiro e mostre o seu antecessor e seu
+// sucessor.
+// Ex:
+// Digite um número: 9
+// O antecessor de 9 é 8
+// O sucessor de 9 é 10
+
+Console.WriteLine("Digite um número inteiro: ");
+int numDigitado = int.Parse(Console.ReadLine());
+int sucessor = numDigitado + 1;
+int antecessor = numDigitado - 1;
+Console.WriteLine("O antecessor é {0}.", antecessor);
+Console.WriteLine("O sucessor é {0}.", sucessor);
