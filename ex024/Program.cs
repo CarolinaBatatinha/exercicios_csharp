4cbe3f27dc89aa630c2ccbe638474a4f93cafd50
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-05-14 14:53:06 -0300

    Add files via upload

---
+++ ex024/Program.cs
@@ -0,0 +1,15 @@
+﻿// 24) Faça um algoritmo que pergunte a distância que um passageiro deseja percorrer em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para viagens até 200Km e R$0.45 para viagens mais longas.
+
+Console.WriteLine("Qual a distância em km que você deseja percorrer?");
+double distancia = Convert.ToDouble(Console.ReadLine());
+
+if (distancia <= 200)
+{
+    double precoViagemCurta = distancia * .5;
+    Console.WriteLine("Para a viagem de {0}km, o preço da passagem é de R${1}.", distancia, precoViagemCurta);
+}
+else
+{
+    double precoViagemLonga = distancia * .45;
+    Console.WriteLine("Para a viagem de {0}km, o preço da passagem é de R${1}.", distancia, precoViagemLonga);
+}
---
+++ ex025 - desafio/Program.cs
@@ -0,0 +1,17 @@
+﻿// 25) [DESAFIO] Crie um programa que leia o tamanho de três segmentos de reta. Analise seus comprimentos e diga se é possível formar um triângulo com essas retas. Matematicamente, para três segmentos formarem um triângulo, o comprimento de cada lado deve ser menor que a soma dos outros dois.
+
+Console.WriteLine("Qual o comprimento do  1º segmento da reta em metros?");
+double primeiroSegmento = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Qual o comprimento do 2º segmento da reta em metros?");
+double segundoSegmento = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Qual o comprimento do 3º segmento da reta em metros?");
+double terceiroSegmento = Convert.ToDouble(Console.ReadLine());
+
+if (primeiroSegmento < segundoSegmento + terceiroSegmento && segundoSegmento < primeiroSegmento + terceiroSegmento && terceiroSegmento < segundoSegmento + primeiroSegmento) {
+    Console.WriteLine("É possível formar um triangulo.");
+}
+else {
+    Console.WriteLine("Não é possível formar um triangulo.");
+}
