5a683a35ae7c9599a1abfbf5f8154bdf91eece51
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-05-16 10:02:08 -0300

    Add files via upload

---
+++ ex034/Program.cs
@@ -0,0 +1,32 @@
+﻿// 34) O Índice de Massa Corpórea (IMC) é um valor calculado baseado na altura e no peso de uma pessoa. De acordo com o valor do IMC, podemos classificar o indivíduo dentro de certas faixas.
+// - abaixo de 18.5: Abaixo do peso
+// - entre 18.5 e 25: Peso ideal
+// - entre 25 e 30: Sobrepeso
+// - entre 30 e 40: Obesidade
+// - acima de 40: Obseidade mórbida
+
+Console.WriteLine("Qual o seu peso?");
+double peso = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Qual a sua altura?");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double imc = peso / (altura * altura);
+
+if (imc < 18.5)
+{
+    Console.WriteLine("Abaixo do peso, IMC: {0:N1}", imc);
+}
+else if (imc >= 18.5 && imc < 25)
+{
+    Console.WriteLine("Peso ideal, IMC: {0:N1}", imc);
+}
+else if (imc >= 25 && imc < 30)
+{
+    Console.WriteLine("Sobrepeso, IMC: {0:N1}", imc);
+}
+else if (imc >= 30 && imc < 40)
+{
+    Console.WriteLine("Obesidade, IMC: {0:N1}", imc);
+} else {
+    Console.WriteLine("Obesidade mórbida, IMC: {0:N1}", imc);
+}
