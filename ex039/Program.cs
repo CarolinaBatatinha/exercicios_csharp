7aebe07c9aa4355efaffbfd75ad5d52e62804758
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-05-16 10:06:29 -0300

    Add files via upload

---
+++ ex039/Program.cs
@@ -0,0 +1,7 @@
+﻿//39) Faça um algoritmo que mostre na tela a seguinte contagem: 10 9 8 7 6 5 4 3 Acabou!
+
+for (int i = 10; i <= 10 && i > 2; i--)
+{
+    Console.Write("{0} ", i);
+}
+Console.WriteLine("Acabou!");
---
+++ ex040/Program.cs
@@ -0,0 +1,7 @@
+﻿// 40) Crie um aplicativo que mostre na tela a seguinte contagem: 0 3 6 9 12 15 18 Acabou!
+
+for (int i = 0; i >= 0 && i < 19; i += 3)
+{
+    Console.Write("{0} ", i);
+}
+Console.Write("Acabou!");
