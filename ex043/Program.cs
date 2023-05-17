f18cfd135200d1cd3f30cba98b5315e73cfe53d1
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-05-17 09:50:22 -0300

    Add files via upload

---
+++ ex043/Program.cs
@@ -0,0 +1,11 @@
+﻿// 43) Desenvolva um algoritmo que mostre uma contagem regressiva de 30 até 1, marcando os números que forem divisíveis por 4, exatamente como mostrado abaixo:
+// 30 29 [28] 27 26 25 [24] 23 22 21 [20] 19 18 17 [16]...
+int i;
+for (i = 30; i <= 30 && i > 0; i--)
+{
+    if (i % 4 == 0)
+    {
+        Console.Write("[{0}] ", i);
+    }
+    Console.Write("{0} ", i);
+}
