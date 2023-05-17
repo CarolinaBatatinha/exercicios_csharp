a20de326d9e0a09565596b6563650d11b68c9401
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-05-17 09:37:19 -0300

    Add files via upload

---
+++ ex041/Program.cs
@@ -0,0 +1,7 @@
+﻿// 41) Desenvolva um programa que mostre na tela a seguinte contagem: 100 95 90 85 80 ... 0 Acabou!
+
+for (int i = 100; i <= 100 && i >= 0; i -= 5)
+{
+    Console.Write("{0} ", i);
+}
+Console.Write("Acabou!");
