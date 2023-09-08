64e8fdbfcf0e7b2f629565c36278650a064d5ab7
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-08 15:12:54 -0300

    Melhoria de código

--- ex042/Program.cs
+++ ex042/Program.cs
@@ -2,7 +2,7 @@
 // Ex: Digite um valor: 35
 // Contagem: 1 2 3 4 5 6 7 ... 33 34 35 Acabou!
 
-Console.WriteLine("Digite um número inteiro e positivo:");
+Console.WriteLine("Digite um número inteiro e positivo: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i <= num; i++)
